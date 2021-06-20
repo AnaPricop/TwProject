5ebff537dacac8282266c8508d32800b896b6e55
olaruandreea12 <39705342+olaruandreea12@users.noreply.github.com>
2021-06-20 21:57:37 +0300

    changed db path

--- src/api/config/db.config.js
+++ src/api/config/db.config.js
@@ -1,10 +1,10 @@
-// Ana: C:\Users\Ana\Desktop\TwProject\data\nobel.db
-// Andreea: C:\Users\Andreea\Documents\Github\TwProject\data\nobel.db
+// Ana: C:\Users\Ana\Desktop\TwProject\data\movies.db
+// Andreea: C:\Users\Andreea\Documents\Github\TwProject\data\movies.db
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize("nobel",null,null,{
     dialect : "sqlite",
-    storage: "C:\\Users\\Andreea\\Documents\\Github\\TwProject\\data\\nobel.db"
+    storage: "C:\\Users\\Andreea\\Documents\\Github\\TwProject\\data\\movies.db"
 });
 
 module.exports = db;
