758a1bac48fbb409928a28ad9023dd57a57b8be2
olaruandreea12 <39705342+olaruandreea12@users.noreply.github.com>
2021-06-22 08:23:01 +0300

    last changes

--- src/api/config/db.config.js
+++ src/api/config/db.config.js
@@ -4,8 +4,8 @@ const { Sequelize } = require('sequelize');
 
 const db = new Sequelize("movies",null,null,{
     dialect : "sqlite",
-    storage: "C:\\Users\\Ana\\Desktop\\TwProject\\data\\movies.db"
-    //  storage: "C:\\Users\\Andreea\\Documents\\Github\\TwProject\\data\\movies.db"
+   // storage: "C:\\Users\\Ana\\Desktop\\TwProject\\data\\movies.db"
+     storage: "C:\\Users\\Andreea\\Documents\\Github\\TwProject\\data\\movies.db"
 });
 
 module.exports = db;
